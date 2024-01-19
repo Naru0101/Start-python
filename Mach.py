33c128fab53145f9dc7eb83c8dc4e89d50402d85
Ihor Kyselov <132590262+Naru0101@users.noreply.github.com>
2024-01-19 13:10:42 +0200

    march

---
+++ Mach.py
@@ -0,0 +1,7 @@
+#Конкулятор.№1.)=
+num1 = float(input("1 number"))
+num2 = float(input("2 number"))
+print("sum num", num1 + num2)
+print("number-", num1 - num2)
+print("number*", num1 * num2)
+print("number/", num1 / num2)
